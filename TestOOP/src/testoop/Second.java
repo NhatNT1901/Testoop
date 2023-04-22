9b085c4e45f4d4d308923a72cf6370aae735578a
NhatNT1901 <tinywormtx@gmail.com>
2023-04-22 11:50:58 +0700

    Hi Third

--- TestOOP/src/testoop/Second.java
+++ TestOOP/src/testoop/Second.java
@@ -12,5 +12,6 @@ public class Second {
     public static void main(String[] args) {
         System.out.println("Second");
         System.out.println("Welcome third");
+        System.out.println("Hi Third, welcome you");
     }
 }
